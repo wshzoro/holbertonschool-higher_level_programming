fa6d9d9d9c5103d6d688480336d7e0e9fb7fcc45
Emma <furlan.emmanuelle@neuf.fr>
2025-05-09 18:24:21 +0200

    import

--- 4-hidden_discovery.py
+++ 4-hidden_discovery.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
+import hidden_4
 
-    for name in sorted(dir(hidden_4)):
+if __name__ == "__main__":
+    print("Module hidden_4 importé avec succès !")
+    
+    # Parcourir toutes les variables définies dans hidden_4
+    for name in dir(hidden_4):
         if not name.startswith("__"):
             print(name)
-
