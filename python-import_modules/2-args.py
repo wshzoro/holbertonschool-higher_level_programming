ccccebb812418222725ebed57ca155796adc3b41
Emma <furlan.emmanuelle@neuf.fr>
2025-05-09 17:26:28 +0200

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arg_count = len(sys.argv) - 1
+
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg_count))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
