be5e71af0712f00a00649e683debff3f42ca1f6b
Emma <furlan.emmanuelle@neuf.fr>
2025-05-12 11:08:02 +0200

    correction

--- 4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-import hidden_4
-
-if __name__ == "__main__":
-    print("Module hidden_4 importé avec succès !")
-    
-    # Parcourir toutes les variables définies dans hidden_4
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
-            print(name)
