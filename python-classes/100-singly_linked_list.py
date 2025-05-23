826c1401dab5d412e764342e24e8715342a8992d
Emma <furlan.emmanuelle@neuf.fr>
2025-05-23 12:30:15 +0200

    advanced task

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defines a Node and a SinglyLinkedList class for sorted insertion."""
+
+
+class Node:
+    """Defines a node of a singly linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize the node with data and optional next_node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieve the data."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Set the data with validation."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve the next node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set the next node with validation."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Defines a singly linked list with sorted insertion."""
+
+    def __init__(self):
+        """Create an empty list with a head."""
+        self.__head = None
+
+    def __str__(self):
+        """Return a string representation of the list."""
+        result = []
+        current = self.__head
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+        return "\n".join(result)
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the list in increasing order."""
+        new_node = Node(value)
+
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
