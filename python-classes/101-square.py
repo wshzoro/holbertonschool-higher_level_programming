fa803461de984dbc96f01152a76b12aeabb4478d
Emma <furlan.emmanuelle@neuf.fr>
2025-05-23 13:30:18 +0200

    task advanced

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -37,13 +37,11 @@ class Square:
     @position.setter
     def position(self, value):
         """Set the position, validating it is a tuple of 2 positive integers."""
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError(
-                "position must be a tuple of 2 positive integers"
-            )
+        if (not isinstance(value, tuple)
+                or len(value) != 2
+                or not all(isinstance(num, int) for num in value)
+                or not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -55,10 +53,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        # Print vertical offset
         for _ in range(self.__position[1]):
             print()
-        # Print each line of the square
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
 
