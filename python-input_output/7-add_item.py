497556d9ffcba3955476e9f02f3b4eca561acc17
Emma <furlan.emmanuelle@neuf.fr>
2025-06-04 12:53:02 +0200

    add item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list,
+and saves them to a file as JSON.
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except Exception:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
