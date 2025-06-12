858b9e14d5e6569a9b665ff51ade2d05ca382254
Emma <furlan.emmanuelle@neuf.fr>
2025-06-12 14:56:28 +0200

    http://localhost:8000

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,17 +1,38 @@
 import http.server
+import socketserver
+import json
 
 PORT = 8000
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
-
-        with socketserver.TCPServer(("", PORT), RequestHandler) as httpd:
-            print("Serving on port, {PORT}")
-            httpd.serve_forever()
-            
-            
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
         
+        elif self.path == "/data":
+            data = {"name": "Jonh", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+with socketserver.TCPServer(("", PORT), RequestHandler) as httpd:
+    print("Serving on port, {PORT}")
+    httpd.serve_forever()
+
+    
