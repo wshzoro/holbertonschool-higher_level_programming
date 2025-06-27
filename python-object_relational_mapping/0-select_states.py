bcee149c4f34989e38004f92fd3e4d1ee999bdc0
Emma <furlan.emmanuelle@neuf.fr>
2025-06-27 17:22:56 +0200

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-import MySQLdb
+import pymysql as MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Récupérer les arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connexion à la base MySQL
     db = MySQLdb.connect(
-        host="host.docker.internal",  # Important sur Mac avec Docker
+        host="127.0.0.1",  # Le container MySQL expose ce port en local
         port=3306,
         user=username,
         passwd=password,
         db=database
     )
 
-    # Préparer un curseur
     cursor = db.cursor()
-
-    # Exécuter la requête SQL pour afficher tous les états triés par id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Afficher les résultats
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
 
-    # Fermer la connexion
     cursor.close()
     db.close()
