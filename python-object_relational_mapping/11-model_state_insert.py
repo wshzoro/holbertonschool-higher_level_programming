c5c2a85da7acddf32bc7d17ccdc5c72da340f78f
Emma <furlan.emmanuelle@neuf.fr>
2025-06-27 22:18:27 +0200

    louisana

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+"""
+Adds the State object "Louisiana" to the database hbtn_0e_6_usa
+"""
+import sys
+import os
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    db_host = os.getenv('HBTN_MYSQL_HOST', 'localhost')
+    db_user = sys.argv[1]
+    db_pwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pwd}@{db_host}:3306/{db_name}',
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
