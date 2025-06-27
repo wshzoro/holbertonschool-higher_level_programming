aa37fb2358449212a787da643e077692d37d63bd
Emma <furlan.emmanuelle@neuf.fr>
2025-06-27 17:39:08 +0200

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-import pymysql
-MySQLdb = pymysql
+""" 0-select_states.py - lists all states from the database hbtn_0e_0_usa """
 
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="127.0.0.1",  # Le container MySQL expose ce port en local
+    conn = MySQLdb.connect(
+        host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cursor.fetchall():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
