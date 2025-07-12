1a4788016e77a5e78b74ce0f7d315ebf011dfcf2
Emma <furlan.emmanuelle@neuf.fr>
2025-07-12 20:05:51 +0200

    task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+#!/usr/bin/node
+const C = 'C is fun';
+const Python = 'Python is cool';
+const JavaScript = 'JavaScript is amazing';
+console.log(C);
+console.log(Python);
+console.log(JavaScript);
