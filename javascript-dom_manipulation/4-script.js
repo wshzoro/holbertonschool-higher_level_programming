72a6b41915eaeb4c80d16f2f6a629f44f30c3b84
Emma <furlan.emmanuelle@neuf.fr>
2025-07-17 10:10:21 +0200

    add item double click

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,5 @@
+document.querySelector('#add_item').addEventListener('click', function () {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+    document.querySelector('.my_list').appendChild(newItem);
+});
